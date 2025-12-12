5950c99aa2b8d1805b10392ce654381f4d0365d9
bekishev <spect420@bk.ru>
2025-12-12 17:30:08 +0300

    удалил лишнее

--- config.py
+++ config.py
@@ -3,17 +3,6 @@
 available_sell_operations = ['sell', 'продать','продала', 'шорт', 'short', 'продал']
 available_buy_operations = ['buy', 'купить', 'купила', 'лонг', 'long','купил']
 
-
-
-# Нужные режими торгов для акций
-BOARDID_SHARES = ['TQBR']
-# Нужные режимы торгов для пифов
-BOARDID_ETFS = ['TQTF']
-
-# Возможно нужно еще расширить (https://iss.moex.com/iss/engines/stock/markets/bonds.xml)
-BOARDID_BONDS = ['TQIR', 'TQOB', 'TQOD', 'TQCB', 'TQOE', 'TQOY', 'TQRD', 'TQUD']
-DEFAULT_BONDS = ['TQRD', 'TQUD']
-
 # Ссылка на API Мосбиржи для сбора данных по акциям
 shares_url = 'https://iss.moex.com/iss/engines/stock/markets/shares/securities.json'
 # Ссылка на API Мосбиржи для сбора данных по облигациям
@@ -26,4 +15,10 @@ currencies_url = 'https://iss.moex.com/iss/engines/currency/markets/index/securi
 urls_settings = {'currency' : ['currency', 'index', 'marketdata_currency', 1, 4, currencies_url],
                  'shares' : ['stock', 'shares', 'marketdataSares', 0, 2, shares_url],
                  'bonds' : ['stock', 'bonds', 'marketdataBonds', 0, 2, bonds_url]}
+
+# Информцация о дроблении / консолидации фондового рынка
+split_url = 'https://iss.moex.com/iss/statistics/engines/stock/splits.json'
+
+# Информация по техническому изменению торговых кодов
+rename_url = 'https://iss.moex.com/iss/history/engines/stock/markets/shares/securities/changeover.json'
 
